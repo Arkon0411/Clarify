e38ce9fd27cd52daca1b991a034c0d5dd2862d85
Arkon <EMJane0411@outlook.com>
2025-11-27 19:14:36 +0800

    fix:
    fixed it finally, it should build normally now

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -607,9 +607,12 @@ function SidebarMenuSkeleton({
   showIcon?: boolean
 }) {
   // Random width between 50 to 90%.
-  const [width] = React.useState(() => {
-    return `${Math.floor(Math.random() * 40) + 50}%`
-  })
+  const id = React.useId()
+  const width = React.useMemo(() => {
+    // Use stable hash from component ID instead of Math.random
+    const hash = id.split(':').reduce((acc, val) => acc + val.charCodeAt(0), 0)
+    return `${(hash % 41) + 50}%`
+  }, [id])
 
   return (
     <div
